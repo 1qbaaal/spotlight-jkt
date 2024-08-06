c6ebd49a5c7c934a66d24e80f26ea0589cd3b500
Mustofa <45399514+Mustofawho0@users.noreply.github.com>
2024-08-06 18:39:50 +0700

    perf: add filter for product and fix pagination

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -2,18 +2,38 @@
 import { useState } from 'react';
 import { samplePage } from '~/contents/sample';
 import { Pagination } from 'antd';
+import { categories } from '~/contents/sample';
 import Image from 'next/image';
 
 export default function Product() {
   const [page, setPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+  const filterSample =
+    selectedCategory === 'All'
+      ? samplePage
+      : samplePage.filter((sample) => sample.category === selectedCategory);
+
   const pageSize = 5;
   const startIndex = (page - 1) * pageSize;
   const endIndex = startIndex + pageSize;
   return (
     <div className='min-h-screen'>
-      <div className='flex items-center justify-center pt-10'>
+      <div className='flex justify-center py-10 gap-10'>
+        {categories.map((c, i) => (
+          <div key={i}>
+            <button
+              className='btn bg-white w-[5vw] border border-gray-800 hover:bg-gray-800 hover:text-white hover:border-black'
+              onClick={() => setSelectedCategory(c)}
+            >
+              {c}
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className='flex items-center justify-center'>
         <div className='grid grid-cols-4 gap-5'>
-          {samplePage.slice(startIndex, endIndex).map((p, i) => (
+          {filterSample.slice(startIndex, endIndex).map((p, i) => (
             <div key={i}>
               <Image
                 src={`${p.imageUrl}`}
@@ -30,7 +50,7 @@ export default function Product() {
       <div className='flex items-center justify-center py-10'>
         <Pagination
           defaultCurrent={1}
-          total={samplePage.length}
+          total={filterSample.length}
           pageSize={pageSize}
           current={page}
           onChange={(page) => setPage(page)}
