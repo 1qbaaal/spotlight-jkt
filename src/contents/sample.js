a4f781b3f7c2d09389f07efc12c44ccf6b106b2f
Mustofa <45399514+Mustofawho0@users.noreply.github.com>
2024-08-06 12:56:12 +0700

    perf: add pagination

---
+++ src/contents/sample.js
@@ -0,0 +1,50 @@
+export const samplePage = [
+  {
+    id: 1,
+    imageUrl: '/pr-1.jpg',
+  },
+  {
+    id: 2,
+    imageUrl: '/pr-2.jpeg',
+  },
+  {
+    id: 3,
+    imageUrl: '/pr-3.jpg',
+  },
+  {
+    id: 4,
+    imageUrl: '/pr-4.jpg',
+  },
+  {
+    id: 5,
+    imageUrl: '/pr-5.jpeg',
+  },
+  {
+    id: 6,
+    imageUrl: '/pr-6.jpeg',
+  },
+  {
+    id: 7,
+    imageUrl: '/pr-7.jpeg',
+  },
+  {
+    id: 8,
+    imageUrl: '/pr-8.jpeg',
+  },
+  {
+    id: 9,
+    imageUrl: '/pr-9.jpeg',
+  },
+  {
+    id: 10,
+    imageUrl: '/pr-10.jpeg',
+  },
+  {
+    id: 11,
+    imageUrl: '/pr-11.jpeg',
+  },
+  {
+    id: 12,
+    imageUrl: '/pr-12.jpeg',
+  },
+];
