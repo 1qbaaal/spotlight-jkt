30f98d7159b1f90279648ce802f22b672d29780d
Mustofa <45399514+Mustofawho0@users.noreply.github.com>
2024-09-07 22:55:09 +0700

    fix: responsive design for detail product

--- src/app/product/[detailProduct]/page.jsx
+++ src/app/product/[detailProduct]/page.jsx
@@ -1,39 +1,61 @@
-"use client";
-import Image from "next/image";
-import DetailProduct from "~/components/detail-product";
-import { productPage } from "~/contents/product";
+'use client';
+import * as React from 'react';
+import Image from 'next/image';
+import DetailProduct from '~/components/detail-product';
+import { productPage } from '~/contents/product';
 
 export default function DetailProductPage(params) {
   const data = productPage.filter((p) => p.id == params.params.detailProduct);
+  const [loading, isLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      isLoading(false);
+    }, 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <div className="relative">
-        <Image
-          src="/2.jpg"
-          alt="logo"
-          width={1000}
-          height={1000}
-          className="w-full h-[50vh] md:h-[70vh] object-cover"
-        />
-        <div className="absolute flex flex-col justify-center items-start w-full h-full pl-5 sm:pl-10">
-          <p className="text-4xl sm:text-5xl font-bold text-white">Detail Product</p>
+    <div className='min-h-screen flex flex-col'>
+      {loading ? (
+        <div className='flex items-center justify-center h-screen bg-white'>
+          <span className='loading loading-spinner loading-lg bg-black'></span>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className='relative'>
+            <Image
+              src='/2.jpg'
+              alt='logo'
+              width={1000}
+              height={1000}
+              loading='lazy'
+              className='w-full h-[50vh] sm:h-[60vh] md:h-[70vh] object-cover'
+            />
+            <div className='absolute flex flex-col justify-center items-start w-full h-full pl-5 top-0'>
+              <p className='text-4xl sm:text-5xl font-bold text-white'>
+                Detail Product
+              </p>
+            </div>
+          </div>
 
-      <div className="py-10 px-5 sm:px-20">
-        <DetailProduct
-          productId={data[0].id}
-          name={data[0].name}
-          product={data[0].product}
-          category={data[0].category}
-          deskripsi={data[0].deskripsi}
-          type={data[0].type}
-          diameter={data[0].dimensi.diameter}
-          tinggi={data[0].dimensi.tinggi}
-          imageUrl={data[0].imageUrl}
-        />
-      </div>
+          <div className='py-10 px-5 sm:px-20'>
+            <DetailProduct
+              productId={data[0].id}
+              name={data[0].name}
+              product={data[0].product}
+              category={data[0].category}
+              deskripsi={data[0].deskripsi}
+              type={data[0].type}
+              diameter={data[0].dimensi.diameter}
+              tinggi={data[0].dimensi.tinggi}
+              imageUrl={data[0].imageUrl}
+            />
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- src/components/detail-product/index.jsx
+++ src/components/detail-product/index.jsx
@@ -1,8 +1,8 @@
-"use client";
-import Image from "next/image";
-import { productPage } from "~/contents/product";
-import Link from "next/link";
-import { descProduct } from "~/contents/product";
+'use client';
+import Image from 'next/image';
+import { productPage } from '~/contents/product';
+import Link from 'next/link';
+import { descProduct } from '~/contents/product';
 
 export default function DetailProduct({
   productId,
@@ -15,77 +15,87 @@ export default function DetailProduct({
   imageUrl,
 }) {
   return (
-    <div className="flex flex-col min-h-screen font-sans">
-      <div className="flex flex-col py-6 px-4 sm:px-8 md:px-12 lg:px-20">
-        <p className="text-2xl sm:text-3xl font-semibold py-2">{name}</p>
-        <hr className="border-2 border-black" />
-        <p className="text-lg sm:text-xl font-semibold py-2">{category}</p>
-        <div className="flex flex-col sm:flex-row gap-6">
-          <div className="w-full sm:w-1/2">
-            <div className="px-4 py-6 flex justify-center">
+    <div className='flex flex-col min-h-screen font-sans'>
+      <div className='flex flex-col py-6 px-4 sm:px-8 md:px-12 lg:px-20'>
+        <p className='text-2xl sm:text-3xl font-semibold py-2'>{name}</p>
+        <hr className='border-2 border-black' />
+        <p className='text-lg sm:text-xl font-semibold py-2'>{category}</p>
+        <div className='flex sm:flex-col lg:flex-row gap-6'>
+          <div className='w-full'>
+            <div className='px-4 py-6 flex justify-center'>
               <Image
                 src={`${imageUrl}`}
-                alt="Product Image"
+                alt='Product Image'
                 width={1000}
                 height={1000}
-                className="w-full max-w-[500px] h-auto"
+                className='w-full max-w-2xl object-cover'
               />
             </div>
           </div>
-          <div className="w-full sm:w-1/2">
-            <div className="px-4 py-6 flex flex-col">
-              <span className="text-lg sm:text-xl font-semibold">Spesifikasi</span>
-              <hr className="border border-black" />
-              <h1 className="text-base sm:text-lg font-semibold pt-4">Tipe:</h1>
-              <span className="text-base sm:text-xl">{type}</span>
-              <h1 className="text-base sm:text-lg font-semibold pt-4">Dimensi Produk:</h1>
+          <div className='w-full'>
+            <div className='px-4 py-6 flex flex-col'>
+              <span className='text-lg sm:text-xl font-semibold'>
+                Spesifikasi
+              </span>
+              <hr className='border border-black' />
+              <h1 className='text-base sm:text-lg font-semibold pt-4'>Tipe:</h1>
+              <span className='text-base sm:text-xl'>{type}</span>
+              <h1 className='text-base sm:text-lg font-semibold pt-4'>
+                Dimensi Produk:
+              </h1>
               <span>Diameter: {diameter}</span>
               <span>Tinggi: {tinggi}</span>
             </div>
-            <div className="px-4 py-6 flex flex-col">
-              <span className="text-lg sm:text-xl font-semibold">Deskripsi</span>
-              <hr className="border border-black" />
-              <span className="text-base sm:text-xl py-4">{name}</span>
+            <div className='px-4 py-6 flex flex-col'>
+              <span className='text-lg sm:text-xl font-semibold'>
+                Deskripsi
+              </span>
+              <hr className='border border-black' />
+              <span className='text-base sm:text-xl py-4'>{name}</span>
               <div>
                 {descProduct.map((d, i) => (
-                  <p key={i} className="text-base sm:text-lg">{d}</p>
+                  <p key={i} className='text-base sm:text-lg'>
+                    {d}
+                  </p>
                 ))}
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div className="py-6 px-4 sm:px-8 md:px-12 lg:px-20">
-        <h1 className="underline text-xl sm:text-2xl font-semibold">Fitur</h1>
-        <h1 className="text-sm sm:text-base py-4">
+      <div className='py-6 px-4 sm:px-8 md:px-12 lg:px-20'>
+        <h1 className='underline text-xl sm:text-2xl font-semibold'>Fitur</h1>
+        <h1 className='text-sm sm:text-base py-4'>
           Harga murah dan kualitas yang jauh lebih baik dari merk kompetitor.
         </h1>
-        <ul className="list-disc pl-5 text-sm sm:text-base">
+        <ul className='list-disc pl-5 text-sm sm:text-base'>
           <li>Garansi sparepart 1 tahun</li>
         </ul>
       </div>
-      <div className="px-4 sm:px-8 md:px-12 lg:px-20">
-        <hr className="border-2 border-black" />
+      <div className='px-4 sm:px-8 md:px-12 lg:px-20'>
+        <hr className='border-2 border-black' />
       </div>
-      <div className="py-6 px-4 sm:px-8 md:px-12 lg:px-20">
-        <h1 className="text-xl sm:text-2xl text-center font-semibold">Other Product</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8 py-6">
+      <div className='py-6 px-4 sm:px-8 md:px-12 lg:px-20'>
+        <h1 className='text-xl sm:text-2xl text-center font-semibold'>
+          Other Product
+        </h1>
+        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 fhd:grid-cols-4 gap-4 sm:gap-6 lg:gap-8 py-6'>
           {productPage.slice(0, 4).map((p, i) => (
-            <div key={i} className="card bg-base-100 w-full max-w-sm shadow-xl">
-              <figure className="px-4 pt-4">
+            <div key={i} className='card bg-base-100 w-full max-w-sm shadow-xl'>
+              <figure className='px-4 pt-4'>
                 <Image
                   src={`${p.imageUrl}`}
-                  alt="Lamp"
-                  className="rounded-xl w-full h-auto"
+                  alt='Lamp'
+                  className='rounded-xl w-[200px] h-[200px] object-cover'
                   height={250}
                   width={250}
                 />
               </figure>
-              <div className="card-body items-center text-center">
-                <h2 className="card-title text-base sm:text-lg">{p.product}</h2>
-                <div className="card-actions">
+              <div className='card-body items-center text-center'>
+                <h2 className='card-title text-base sm:text-lg'>{p.product}</h2>
+                <div className='card-actions'>
                   <Link href={`/product/${p.id}`}>
-                    <button className="border-2 bg-slate-200 px-4 py-2 rounded-lg shadow-lg hover:bg-black hover:text-white">
+                    <button className='border-2 bg-slate-200 px-4 py-2 rounded-lg shadow-lg hover:bg-black hover:text-white'>
                       View Product
                     </button>
                   </Link>
